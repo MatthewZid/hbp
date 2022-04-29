3538960adfbbea0abdaf94a5d6f613bb88ee7497
MatthewZid <m_zidianakis@hotmail.com>
2022-04-29 18:14:28 +0300

    first commit

---
+++ behavioural_cloning.py
@@ -0,0 +1,186 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
+import tensorflow as tf
+from tensorflow.python.keras.layers import Input, Dense, ReLU, Concatenate, LeakyReLU, Add
+from tensorflow.python.keras.models import Model
+import numpy as np
+from utils import *
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from tqdm import trange
+
+BATCH_SIZE = 2048
+EPOCHS = 10000
+K = 10
+show_fig = True
+
+def create_generator(state_dims, action_dims, code_dims):
+    initializer = tf.keras.initializers.GlorotNormal()
+    states = Input(shape=state_dims)
+    # default: x = Dense(300, kernel_initializer=initializer, activation='tanh')(states)
+    x = Dense(260, kernel_initializer=initializer, activation='tanh')(states)
+    # x = LeakyReLU()(x)
+    codes = Input(shape=code_dims)
+    c = Dense(64, kernel_initializer=initializer, activation='tanh')(codes)
+    # c = LeakyReLU()(c)
+    h = Concatenate(axis=1)([x,c])
+    actions = Dense(action_dims)(h)
+
+    model = Model(inputs=[states,codes], outputs=actions)
+    return model
+
+# load data
+expert_data = read_expert()
+features = extract_features(expert_data)
+
+# standardize(features['states'])
+# standardize(features['actions'])
+# normalize(features['states'])
+# normalize(features['actions'])
+
+def train(train_states, train_actions, train_codes, val_states, val_actions, val_codes):
+    train_states = tf.convert_to_tensor(train_states, dtype=tf.float32)
+    train_actions = tf.convert_to_tensor(train_actions, dtype=tf.float32)
+    train_codes = tf.convert_to_tensor(train_codes, dtype=tf.float32)
+    val_states = tf.convert_to_tensor(val_states, dtype=tf.float32)
+    val_actions = tf.convert_to_tensor(val_actions, dtype=tf.float32)
+    val_codes = tf.convert_to_tensor(val_codes, dtype=tf.float32)
+
+    train_data = tf.data.Dataset.from_tensor_slices((train_states, train_actions, train_codes))
+    train_data = train_data.batch(batch_size=BATCH_SIZE)
+
+    val_data = tf.data.Dataset.from_tensor_slices((val_states, val_actions, val_codes))
+    val_data = val_data.batch(batch_size=BATCH_SIZE)
+
+    # train
+    generator = create_generator(features['states'].shape[1], features['actions'].shape[1], features['codes'].shape[1])
+
+    # gen_optimizer = tf.keras.optimizers.SGD(learning_rate=1e-4)
+    gen_optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    mse = tf.keras.losses.MeanSquaredError()
+
+    epochs = EPOCHS
+    total_train_size = sum([el[0].shape[0] for el in list(train_data.as_numpy_iterator())])
+    total_val_size = sum([el[0].shape[0] for el in list(val_data.as_numpy_iterator())])
+    result_train = []
+    result_val = []
+    for epoch in trange(epochs, desc='Epoch'):
+        loss = 0.0
+        for _, (states_batch, actions_batch, codes_batch) in enumerate(train_data):
+            with tf.GradientTape() as gen_tape:
+                actions_mu = generator([states_batch, codes_batch], training=True)
+                gen_loss = mse(actions_batch, actions_mu)
+            
+            policy_gradients = gen_tape.gradient(gen_loss, generator.trainable_weights)
+            gen_optimizer.apply_gradients(zip(policy_gradients, generator.trainable_weights))
+
+            loss += tf.get_static_value(gen_loss) * states_batch.shape[0]
+        
+        epoch_loss = loss / total_train_size
+        result_train.append(epoch_loss)
+
+        loss = 0.0
+        for _, (states_batch, actions_batch, codes_batch) in enumerate(val_data):
+            actions_mu = generator([states_batch, codes_batch], training=False)
+            gen_loss = mse(actions_batch, actions_mu)
+            loss += tf.get_static_value(gen_loss) * states_batch.shape[0]
+        
+        epoch_loss = loss / total_val_size
+        result_val.append(epoch_loss)
+    
+    return generator, result_train, result_val
+
+def train_kfold():
+    # initialize k-fold cross validation
+    kf = KFold(n_splits=K, shuffle=False, random_state=None)
+    avg_train_loss = np.zeros((EPOCHS,), dtype=np.float32)
+    avg_val_loss = np.zeros((EPOCHS,), dtype=np.float32)
+    best_avg_loss = np.inf
+    best_gen = None
+    mse = tf.keras.losses.MeanSquaredError()
+
+    idx = np.arange(features['states'].shape[0])
+    np.random.shuffle(idx)
+    shuffled_expert_states = features['states'][idx]
+    shuffled_expert_actions = features['actions'][idx]
+    shuffled_expert_codes = features['codes'][idx]
+
+    train_ratio = int((80*features['states'].shape[0])/100.0)
+    train_states = shuffled_expert_states[0:train_ratio, :]
+    test_states = shuffled_expert_states[train_ratio:, :]
+    train_actions = shuffled_expert_actions[0:train_ratio, :]
+    test_actions = shuffled_expert_actions[train_ratio:, :]
+    train_codes = shuffled_expert_codes[0:train_ratio, :]
+    test_codes = shuffled_expert_codes[train_ratio:, :]
+
+    for train_index, test_index in kf.split(train_states):
+        states_train, states_test = train_states[train_index], train_states[test_index]
+        actions_train, actions_test = train_actions[train_index], train_actions[test_index]
+        codes_train, codes_test = train_codes[train_index], train_codes[test_index]
+
+        generator, result_train, result_val = train(states_train, actions_train, codes_train, states_test, actions_test, codes_test)
+        avg_train_loss += np.array(result_train) / float(K)
+        avg_val_loss += np.array(result_val) / float(K)
+
+        if (sum(result_val)/float(len(result_val))) < best_avg_loss:
+            best_avg_loss = sum(result_val)/float(len(result_val))
+            best_gen = generator
+    
+    test_actions_mu = best_gen([test_states, test_codes], training=False)
+    test_gen_loss = mse(test_actions, test_actions_mu)
+
+    return best_gen, avg_train_loss, avg_val_loss, test_gen_loss
+
+def simple_train():
+    # pretrain with behavioural cloning
+    idx = np.arange(features['states'].shape[0])
+    np.random.shuffle(idx)
+    shuffled_expert_states = features['states'][idx]
+    shuffled_expert_actions = features['actions'][idx]
+    shuffled_expert_codes = features['codes'][idx]
+
+    train_ratio = int((80*features['states'].shape[0])/100.0)
+    train_states = shuffled_expert_states[0:train_ratio, :]
+    val_states = shuffled_expert_states[train_ratio:, :]
+    train_actions = shuffled_expert_actions[0:train_ratio, :]
+    val_actions = shuffled_expert_actions[train_ratio:, :]
+    train_codes = shuffled_expert_codes[0:train_ratio, :]
+    val_codes = shuffled_expert_codes[train_ratio:, :]
+
+    # standardize states/actions
+    state_normalizer = MinMaxScaler(feature_range=(-1,1))
+    action_normalizer = MinMaxScaler(feature_range=(-1,1))
+
+    train_states = state_normalizer.fit_transform(train_states)
+    train_actions = action_normalizer.fit_transform(train_actions)
+    val_states = state_normalizer.transform(val_states)
+    val_actions = action_normalizer.transform(val_actions)
+
+    return train(train_states, train_actions, train_codes, val_states, val_actions, val_codes)
+
+generator = None
+result_test = None
+result_train = None
+result_val = None
+
+test_flag = False
+if test_flag:
+    generator, result_train, result_val, result_test = train_kfold()
+    print('\nTest loss: {:f}'.format(result_test))
+else: generator, result_train, result_val = simple_train()
+
+if show_fig:
+    epoch_space = np.arange(1, len(result_train)+1, dtype=int)
+    plt.figure()
+    plt.title('Behaviour Cloning')
+    plt.plot(epoch_space, result_train)
+    plt.plot(epoch_space, result_val)
+    plt.legend(['train loss', 'validation loss'], loc="upper right")
+    plt.savefig('./plots/behaviour_cloning', dpi=100)
+    plt.close()
+
+generator.save_weights('./saved_models/bc/generator.h5')
+print('\nGenerator saved!') 
---
+++ utils.py
@@ -0,0 +1,77 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+OBJ_SIZE_POS = 4
+
+def read_expert(dataset_name='preprocessed_2D'):
+    # data path
+    basepath = '/home/matthew/Documents/AI/thesis/hbp/dataset'
+    datapath = os.path.join(basepath, dataset_name)
+
+    # init dataset dict
+    dataset = {}
+
+    # read csv's
+    print('Reading expert dataset...')
+    datalist = [os.path.join(datapath, dirname) for dirname in os.listdir(datapath) if os.path.isdir(os.path.join(datapath, dirname))]
+    for dl in datalist:
+        for datafile in os.listdir(dl):
+            fl = os.path.join(dl, datafile)
+            dataset[datafile] = pd.read_csv(fl, float_precision='round_trip')
+    
+    return dataset
+
+def get_coords(df, keep=2, start=3):
+    sk = []
+    for i in range(start, len(df.columns)):
+        if keep != 0:
+            sk.append(df.iloc[:,i])
+            keep -= 1
+        else: keep = 2
+    
+    return pd.concat(sk, axis=1)
+
+def standardize(array):
+    for i in range(array.shape[1]):
+        array[:,i] = (array[:,i] - array[:,i].mean()) / array[:,i].std()
+
+def normalize(array):
+    for i in range(array.shape[1]):
+        array[:,i] = (array[:,i] - array[:,i].min()) / (array[:,i].max() - array[:,i].min())
+
+def plot_feature(feature, type='actions'):
+    plt.figure()
+    plt.title('Expert '+type)
+    for i in range(0, feature.shape[1]-1, 2):
+        plt.scatter(feature[:,i], feature[:,i+1], s=4, alpha=0.4)
+    plt.savefig('./plots/'+type, dpi=100)
+    plt.close()
+
+def extract_features(dataset):
+    one_hot = {'S': [1,0,0], 'M': [0,1,0], 'L': [0,0,1]}
+    features = {}
+    features['states'] = []
+    features['actions'] = []
+    features['codes'] = []
+
+    for key in dataset.keys():
+        states = get_coords(dataset[key])
+        actions = []
+
+        for i in range(len(states)-1):
+            actions.append(states.iloc[i+1] - states.iloc[i])
+
+        actions = pd.concat(actions, ignore_index=True, axis=1).T
+        states = states.drop(len(states.index)-1)
+
+        features['states'].append(states)
+        features['actions'].append(actions)
+        features['codes'].append(np.array([one_hot[key[OBJ_SIZE_POS]] for _ in range(len(states))]))
+    
+    features['states'] = pd.concat(features['states'], ignore_index=True).to_numpy()
+    features['actions'] = pd.concat(features['actions'], ignore_index=True).to_numpy()
+    features['codes'] = np.concatenate(features['codes'], axis=0)
+
+    return features
