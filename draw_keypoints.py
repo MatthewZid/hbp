8d22e55d3cd1b63edb28ac86795ace926d0a3af3
MatthewZid <m_zidianakis@hotmail.com>
2022-05-05 03:14:39 +0300

    exports video with keypoint trajectories

--- draw_keypoints.py
+++ draw_keypoints.py
@@ -6,60 +6,72 @@ import numpy as np
 IMAGEWIDTH = 640.0
 IMAGEHEIGHT = 480.0
 
+def draw_points(states, filename, width, height):
+    name = filename.split('.')[0]
+    video_out = cv2.VideoWriter(name+'.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 6, (width, height))
+
+    for i in range(states.shape[0]):
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+
+        for j in range(0, states.shape[1], 2):
+            c_coords = (states[i, j], states[i, j+1])
+            radius = 3
+            color = (51, 51, 255)
+            thickness = -1
+            image = cv2.circle(image, c_coords, radius, color, thickness)
+
+        # connect thumb
+        image = cv2.line(image, (states[i,2], states[i,3]), (states[i,4], states[i,5]), (51, 51, 255), 1)
+        image = cv2.line(image, (states[i,4], states[i,5]), (states[i,6], states[i,7]), (51, 51, 255), 1)
+        image = cv2.line(image, (states[i,6], states[i,7]), (states[i,8], states[i,9]), (51, 51, 255), 1)
+        image = cv2.line(image, (states[i,8], states[i,9]), (states[i,10], states[i,11]), (51, 51, 255), 1)
+
+        # connect index
+        image = cv2.line(image, (states[i,2], states[i,3]), (states[i,12], states[i,13]), (0, 255, 128), 1)
+        image = cv2.line(image, (states[i,12], states[i,13]), (states[i,14], states[i,15]), (0, 255, 128), 1)
+        image = cv2.line(image, (states[i,14], states[i,15]), (states[i,16], states[i,17]), (0, 255, 128), 1)
+        image = cv2.line(image, (states[i,16], states[i,17]), (states[i,18], states[i,19]), (0, 255, 128), 1)
+
+        # connect middle
+        image = cv2.line(image, (states[i,2], states[i,3]), (states[i,20], states[i,21]), (0, 153, 76), 1)
+        image = cv2.line(image, (states[i,20], states[i,21]), (states[i,22], states[i,23]), (0, 153, 76), 1)
+        image = cv2.line(image, (states[i,22], states[i,23]), (states[i,24], states[i,25]), (0, 153, 76), 1)
+        image = cv2.line(image, (states[i,24], states[i,25]), (states[i,26], states[i,27]), (0, 153, 76), 1)
+
+        # connect ring
+        image = cv2.line(image, (states[i,2], states[i,3]), (states[i,28], states[i,29]), (255, 0, 0), 1)
+        image = cv2.line(image, (states[i,28], states[i,29]), (states[i,30], states[i,31]), (255, 0, 0), 1)
+        image = cv2.line(image, (states[i,30], states[i,31]), (states[i,32], states[i,33]), (255, 0, 0), 1)
+        image = cv2.line(image, (states[i,32], states[i,33]), (states[i,34], states[i,35]), (255, 0, 0), 1)
+
+        # connect pinky
+        image = cv2.line(image, (states[i,2], states[i,3]), (states[i,36], states[i,37]), (255, 0, 255), 1)
+        image = cv2.line(image, (states[i,36], states[i,37]), (states[i,38], states[i,39]), (255, 0, 255), 1)
+        image = cv2.line(image, (states[i,38], states[i,39]), (states[i,40], states[i,41]), (255, 0, 255), 1)
+        image = cv2.line(image, (states[i,40], states[i,41]), (states[i,42], states[i,43]), (255, 0, 255), 1)
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        video_out.write(image)
+
+    video_out.release()
+
 expert_data = read_expert()
+features = extract_features(expert_data)
+state_normalizer = MinMaxScaler(feature_range=(0,1))
+state_normalizer.fit(features['states'])
 states = None
 
 for key in expert_data.keys():
         states = get_coords(expert_data[key]).to_numpy()
-        state_normalizer = MinMaxScaler(feature_range=(0,1))
-        states = state_normalizer.fit_transform(states)
+        states = state_normalizer.transform(states)
 
         for i in range(1, states.shape[1], 2):
-            states[:, i] = states[:, i] * IMAGEWIDTH
-        
-        for i in range(0, states.shape[1], 2):
             states[:, i] = states[:, i] * IMAGEHEIGHT
         
+        for i in range(0, states.shape[1], 2):
+            states[:, i] = states[:, i] * IMAGEWIDTH
+        
         states = np.around(states).astype(int)
+
+        draw_points(states, key, int(IMAGEWIDTH), int(IMAGEHEIGHT))
         break
-
-image = np.zeros((480, 640, 3), dtype=np.uint8)
-
-for i in range(0, states.shape[1], 2):
-    c_coords = (states[0, i], states[0, i+1])
-    radius = 3
-    color = (51, 51, 255)
-    thickness = -1
-    image = cv2.circle(image, c_coords, radius, color, thickness)
-
-# connect thumb
-image = cv2.line(image, (states[0,2], states[0,3]), (states[0,4], states[0,5]), (51, 51, 255), 1)
-image = cv2.line(image, (states[0,4], states[0,5]), (states[0,6], states[0,7]), (51, 51, 255), 1)
-image = cv2.line(image, (states[0,6], states[0,7]), (states[0,8], states[0,9]), (51, 51, 255), 1)
-image = cv2.line(image, (states[0,8], states[0,9]), (states[0,10], states[0,11]), (51, 51, 255), 1)
-
-# connect index
-image = cv2.line(image, (states[0,2], states[0,3]), (states[0,12], states[0,13]), (0, 255, 128), 1)
-image = cv2.line(image, (states[0,12], states[0,13]), (states[0,14], states[0,15]), (0, 255, 128), 1)
-image = cv2.line(image, (states[0,14], states[0,15]), (states[0,16], states[0,17]), (0, 255, 128), 1)
-image = cv2.line(image, (states[0,16], states[0,17]), (states[0,18], states[0,19]), (0, 255, 128), 1)
-
-# connect middle
-image = cv2.line(image, (states[0,2], states[0,3]), (states[0,20], states[0,21]), (0, 153, 76), 1)
-image = cv2.line(image, (states[0,20], states[0,21]), (states[0,22], states[0,23]), (0, 153, 76), 1)
-image = cv2.line(image, (states[0,22], states[0,23]), (states[0,24], states[0,25]), (0, 153, 76), 1)
-image = cv2.line(image, (states[0,24], states[0,25]), (states[0,26], states[0,27]), (0, 153, 76), 1)
-
-# connect ring
-image = cv2.line(image, (states[0,2], states[0,3]), (states[0,28], states[0,29]), (255, 0, 0), 1)
-image = cv2.line(image, (states[0,28], states[0,29]), (states[0,30], states[0,31]), (255, 0, 0), 1)
-image = cv2.line(image, (states[0,30], states[0,31]), (states[0,32], states[0,33]), (255, 0, 0), 1)
-image = cv2.line(image, (states[0,32], states[0,33]), (states[0,34], states[0,35]), (255, 0, 0), 1)
-
-# connect pinky
-image = cv2.line(image, (states[0,2], states[0,3]), (states[0,36], states[0,37]), (255, 0, 255), 1)
-image = cv2.line(image, (states[0,36], states[0,37]), (states[0,38], states[0,39]), (255, 0, 255), 1)
-image = cv2.line(image, (states[0,38], states[0,39]), (states[0,40], states[0,41]), (255, 0, 255), 1)
-image = cv2.line(image, (states[0,40], states[0,41]), (states[0,42], states[0,43]), (255, 0, 255), 1)
-
-cv2.imwrite('image.jpg', image)
