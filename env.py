56ea39fdc9ff3f9751237a99eb221b6908a153c6
MatthewZid <m_zidianakis@hotmail.com>
2022-05-24 22:34:09 +0300

    corrected trajectory stop criteria

--- env.py
+++ env.py
@@ -11,8 +11,7 @@ class Env():
 		self.state    = None
 		self.max_step = max_step
 		self.n_step   = 1
-		self.frame_count = 0
-		self.std10 = []
+		self.stop_crit = None
 		self.sigma1   = sigma1
 		self.sigma2   = sigma2
 		self.speed    = speed
@@ -30,20 +29,10 @@ class Env():
 			self.state[i, :] = self.state[i+1, :]
 		self.state[4] = np.copy(self.p)
 
-		self.std10.append(self.state[-1,-1])
-		self.frame_count += 1
-
-		stdy = np.inf
-		if self.frame_count == 10:
-			stdy = np.array(self.std10).std()
-		elif self.frame_count > 10:
-			self.std10.pop(0)
-			stdy = np.array(self.std10).std()
-
 		reward = 0.0
 
 		# if self.n_step >= 129 or abs(self.p[0]) >= 1 or abs(self.p[1]) >= 1:
-		if stdy <= 0.033:	# must determine stop criteria with object distance (here: last std from std10 must be <= 1.5 for unnormalized y-wrist feature or 0.033 for normalized apertures)
+		if self.p[-1] >= self.stop_crit:	# must determine stop criteria with object distance (here: if y-wrist coord >= stop_crit, stop)
 			done = True
 		else:
 			done = False
@@ -54,7 +43,7 @@ class Env():
 	#-------------------------
 	# Reset
 	#-------------------------
-	def reset(self, start=(0.0,0.0,0.0)):
+	def reset(self, code, start=(0.0,0.0,0.0)):
 		self.state = np.zeros((5, len(start)), dtype=np.float64)
 		self.p = np.zeros((len(start),), dtype=np.float64)
 		for i in range(len(start)): self.p[i] = start[i]
@@ -64,7 +53,9 @@ class Env():
 			for j in range(len(start)):
 				self.state[i,j] = self.p[j]
 		
-		self.std10.append(self.state[-1,-1])
-		self.frame_count += 1
+		# set trajectory stop criteria (y-wrist)
+		if code[0] == 1: self.stop_crit = 168.6 # small
+		elif code[1] == 1: self.stop_crit = 165.11 # medium
+		elif code[2] == 1: self.stop_crit = 168.55 # large
 
 		return np.copy(self.state.flatten())
